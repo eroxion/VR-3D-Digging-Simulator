1e57506b790d8f2f502b31040d86e4ac9195b24c
Anusheel Soni <anusheelsoni4@gmail.com>
2025-03-31 20:43:01 +0530

    Updates
    - Modified terrain.
    - Added digging animations.

--- Assets/Scripts/Dig Simulator/ShovelController.cs
+++ Assets/Scripts/Dig Simulator/ShovelController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Dig_Simulator
@@ -6,18 +7,35 @@ namespace Dig_Simulator
     public class ShovelController : MonoBehaviour
     {
         private XRIDefaultInputActions inputSystem;
+        private const string IS_DIGGING = "Dig";
+        private bool diggingBool = false;
+        private Animator animator;
+        
         private void Awake()
         {
+            animator = GetComponent<Animator>();
             inputSystem = new XRIDefaultInputActions();
             inputSystem.XRIRightInteraction.Enable();
         }
 
+        private void Update(){
+            animator.SetBool(IS_DIGGING, diggingBool);
+        }
+        
         private void OnTriggerStay(Collider other)
         {
-            if (other.CompareTag("Mud") && inputSystem.XRIRightInteraction.Activate.IsPressed())
+            if (other.CompareTag("Mud") && inputSystem.XRIRightInteraction.Activate.IsPressed() && diggingBool == false)
             {
+                diggingBool = true;
                 Destroy(other.gameObject);
+                StartCoroutine(diggingDelay());
             }
+        }
+
+        private IEnumerator diggingDelay()
+        {
+            yield return new WaitForSeconds(1f);
+            diggingBool = false;
         }
     }
 }
